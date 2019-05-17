53d07c88088e55cebcb109a2123ebd08adafd869
Ridwan Farouki <ridwan@simprints.com>
2019-05-17 06:03:43 +0100

    Updated transform

--- src/main/java/com/ridwanstandingby/verve/render/Camera.kt
+++ src/main/java/com/ridwanstandingby/verve/render/Camera.kt
@@ -13,15 +13,18 @@ class Camera(
 
     private val screenOrigin = FloatVector2(screenDimension.x / 2, screenDimension.y / 2)
 
-    private var direction: Quaternion = initialDirection
-        set(value) {
+    var transform: Quaternion = initialDirection
+        private set(value) {
             field = value
-            rightDirection = CAMERA_FRAME_RIGHT.rotate(direction)
+            direction = CAMERA_FRAME_FORWARD.rotate(transform)
+            rightDirection = CAMERA_FRAME_RIGHT.rotate(transform)
             leftDirection = rightDirection * -1.0
-            upDirection = CAMERA_FRAME_UP.rotate(direction)
+            upDirection = CAMERA_FRAME_UP.rotate(transform)
             downDirection = upDirection * -1.0
         }
 
+    var direction: Vector3 = CAMERA_FRAME_FORWARD
+        private set
     var rightDirection: Vector3 = CAMERA_FRAME_RIGHT
         private set
     var leftDirection: Vector3 = CAMERA_FRAME_RIGHT * -1.0
@@ -31,19 +34,21 @@ class Camera(
     var downDirection: Vector3 = CAMERA_FRAME_UP * -1.0
         private set
 
-    fun transform(q: Quaternion) {
-        direction = rotationTransformer(q)
+    fun updateTransform(q: Quaternion) {
+        transform = rotationTransformer(q)
     }
 
-    fun project(u: Vector3): FloatVector2 =
-        u.rotate(direction.inverse()) // The inverse is used to rotate everything else in the view to the Camera's frame
-            .let { v ->
-                FloatVector2(v.x, -v.y) + screenOrigin // Note that camY is measured from the top of the screen
-            }
+    fun transform(v: Vector3) =
+        v.rotate(transform.inverse()) // The inverse is used to rotate everything else in the view to the Camera's frame
+
+    fun project(v: Vector3): FloatVector2 =
+        transform(v).let {
+            FloatVector2(it.x, -it.y) + screenOrigin // Note that camY is measured from the top of the screen
+        }
 
     companion object {
         // The camera is looking down the z axis towards minus infinity in the camera frame
-        private val CAMERA_FRAME_FORWARD = Vector3(0.0, 0.0, -1.0)
+        val CAMERA_FRAME_FORWARD = Vector3(0.0, 0.0, -1.0)
         private val CAMERA_FRAME_RIGHT = Vector3(1.0, 0.0, 0.0)
         private val CAMERA_FRAME_UP = Vector3(0.0, 1.0, 0.0)
         private val CAMERA_FRAME_IDENTITY_TRANSFORM = Quaternion(0.0, CAMERA_FRAME_FORWARD)
