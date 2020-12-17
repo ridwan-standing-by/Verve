80f40a0a01fe9dc0458233c69e010e27a36c2cd1
Ridwan Farouki <arfarouki@gmail.com>
2020-12-17 14:36:16 +0000

    More functions

--- src/main/java/com/ridwanstandingby/verve/math/Functions.kt
+++ src/main/java/com/ridwanstandingby/verve/math/Functions.kt
@@ -1,3 +1,8 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
 package com.ridwanstandingby.verve.math
 
-fun Float.sq() = this * this
+inline fun Float.sq() = this * this
+inline fun Double.sq() = this * this
+
+inline fun Double.cubed() = this * this * this
--- src/main/java/com/ridwanstandingby/verve/math/Vector3.kt
+++ src/main/java/com/ridwanstandingby/verve/math/Vector3.kt
@@ -1,3 +1,5 @@
+@file:Suppress("NOTHING_TO_INLINE", "unused", "MemberVisibilityCanBePrivate")
+
 package com.ridwanstandingby.verve.math
 
 import kotlin.math.acos
@@ -11,12 +13,15 @@ data class Vector3(val x: Double, val y: Double, val z: Double) {
     operator fun times(k: Double) = Vector3(x * k, y * k, z * k)
     operator fun unaryMinus() = Vector3(-x, -y, -z)
     infix fun dot(o: Vector3) = x * o.x + y * o.y + z * o.z
+    infix fun cross(o: Vector3) = Vector3(y * o.z - z * o.y, z * o.x - x * o.z, x * o.y - y * o.x)
 
     fun rotate(q: Quaternion): Vector3 = (q * (this.toQuaternion() * q.inverse())).axis()
 
     fun reflect(n: Vector3): Vector3 = this + n * ((this dot n) * -2.0)
 
-    fun toSphericalVector2() = normalise().let { SphericalVector2(atan2(it.y, it.x), acos(it.z / it.size())) }
+    fun toSphericalVector2() =
+        normalise().let { SphericalVector2(atan2(it.y, it.x), acos(it.z / it.size())) }
+
     private fun toQuaternion() = Quaternion(0.0, x, y, z)
 
     fun normalise() = size().let { norm ->
@@ -29,3 +34,10 @@ data class Vector3(val x: Double, val y: Double, val z: Double) {
         val O = Vector3(0.0, 0.0, 0.0)
     }
 }
+
+inline fun dist(a: Vector3, b: Vector3): Double =
+    sqrt((a.x - b.x).sq() + (a.y - b.y).sq() + (a.z - b.z).sq())
+
+inline fun dist2(a: Vector3, b: Vector3): Double =
+    (a.x - b.x).sq() + (a.y - b.y).sq() + (a.z - b.z).sq()
+
