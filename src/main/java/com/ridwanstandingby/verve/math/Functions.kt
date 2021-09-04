b82653761b642997ea832997267d6e6c50cb01e4
ridwan-standing-by <arfarouki@gmail.com>
2021-09-04 11:04:59 +0100

    Toroidal distance calculation

--- src/main/java/com/ridwanstandingby/verve/math/Functions.kt
+++ src/main/java/com/ridwanstandingby/verve/math/Functions.kt
@@ -3,6 +3,7 @@
 package com.ridwanstandingby.verve.math
 
 import com.ridwanstandingby.verve.tools.Api
+import kotlin.math.abs
 
 @Api
 inline fun Float.sq() = this * this
@@ -12,3 +13,19 @@ inline fun Double.sq() = this * this
 
 @Api
 inline fun Double.cubed() = this * this * this
+
+/** Calculates the smallest [a] - [b] if both belong on a toroidal field with [period] */
+@Api
+inline fun toroidalDiff(a: Double, b: Double, period: Double): Double {
+    val ba = a - b
+    val baAlt = if (ba > 0) {
+        ba - period
+    } else {
+        ba + period
+    }
+    return if (abs(ba) > abs(baAlt)) {
+        baAlt
+    } else {
+        ba
+    }
+}
